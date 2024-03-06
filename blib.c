99943a38dc06c081d5ca9d38c896dd3c94fd4281
hjzts <119279415+hjzts@users.noreply.github.com>
2024-03-06 20:26:16 +0800

    c

--- blib.c
+++ blib.c
@@ -101,7 +101,7 @@ char* strsep(char** stringp, const char* delim)
     }
     token = *stringp;
     while (**stringp != '\0') {
-        if (strchr(delim, (int)**stringp)) {
+        if (strchr(delim, (int)**stringp) != NULL) {
             **stringp == '\0';
             (*stringp)++;
             return token;
