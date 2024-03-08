6717282ab6fdc7eed984fcdd334d256003a754c3
胡健 <22373058@buaa.edu.cn>
2024-03-08 13:20:59 +0800

    hw3'

--- ray/sh_test2/search.sh
+++ ray/sh_test2/search.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #First you can use grep (-n) to find the number of lines of string.
 #Then you can use awk to separate the answer.
-grep -n int file > 
+grep -n int file | awk -F : '{print$1}'  
