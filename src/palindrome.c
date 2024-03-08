ed8c4a57150fc72c8b4032cc205d1b4f82e0ed3e
hjzts <119279415+hjzts@users.noreply.github.com>
2024-03-08 11:57:19 +0800

    first

--- src/palindrome.c
+++ src/palindrome.c
@@ -1,12 +1,31 @@
 #include <stdio.h>
-int main() {
-	int n;
-	scanf("%d", &n);
+int main()
+{
+    int n;
+    scanf("%d", &n);
 
-	if () {
-		printf("Y\n");
-	} else {
-		printf("N\n");
-	}
-	return 0;
+    int flag = 1;
+    int s[10];
+    int len = 0;
+    for (len = 0; n; len++) {
+        s[len] = n % 10;
+        n /= 10;
+    }
+    int i = 0, j = len - 1;
+    while (i < j) {
+        if (s[i] == s[j]) {
+            i++;
+            j--;
+        } else {
+            flag = 0;
+            break;
+        }
+    }
+
+    if (flag) {
+        printf("Y\n");
+    } else {
+        printf("N\n");
+    }
+    return 0;
 }
--- src/sh_test/hello_os.sh
+++ src/sh_test/hello_os.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
+src_file="$1"
+dst_file="$2"
+
+sed -n '8p' ${src_file} > ${dst_file}
+sed -n '32p' ${src_file} >> ${dst_file}
+sed -n '128p' ${src_file} >> ${dst_file}
+sed -n '512p' ${src_file} >> ${dst_file}
+sed -n '1024p' ${src_file} >> ${dst_file}
+
 
