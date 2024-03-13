2e8094dcb6380a0d359c762d404f597a5e31ebd6
胡健 <22373058@buaa.edu.cn>
2024-03-13 21:30:09 +0800

    f

--- handle-ps.sh
+++ handle-ps.sh
@@ -40,7 +40,7 @@ elif [ ! -z "$CMD" ]; then
 	cat $FILE | grep $CMD
 elif [ ! -z $PID ]; then
     # Your code here. (3/3)
-	while [ "$PID" != "0" ]
+	while [ $PID != "0" ]
 	do
 		awk -v input=$PID '$2==input {print $3;}' $FILE > tmp.txt
 		echo "PID=" > tmp.sh
