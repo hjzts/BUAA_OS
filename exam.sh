a78bb7596877c63a8ad1c9aa232d25701db8268b
胡健 <22373058@buaa.edu.cn>
2024-03-13 20:36:20 +0800

    aaaa

--- exam.sh
+++ exam.sh
@@ -23,13 +23,14 @@ chmod 655 err.txt
 n1=1
 n2=1
 
-i=1
-while [ $i -le $# ]
-do
-	n$i=$${i}
-	i=$((i+1))
-done
+if [ $# -eq 1 ]; then
+	n1=$1
+fi
 
+if [ $# -eq 2 ]; then
+	n1=$1
+	n2=$2
+fi
 n3=$[$n1+$n2]
 tmp=${n3}p
 #echo $tmp
