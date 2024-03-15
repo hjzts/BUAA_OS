a996209facdf7199fb8f89f80e13838078e7db65
胡健 <22373058@buaa.edu.cn>
2024-03-15 08:35:43 +0800

    -ngit add .yihan

--- handle-ps.sh
+++ handle-ps.sh
@@ -40,10 +40,10 @@ elif [ ! -z "$CMD" ]; then
 	cat $FILE | grep $CMD
 elif [ ! -z $PID ]; then
     # Your code here. (3/3)
-	while [ $PID != "0" ]
+	while [ $PID -gt 0 ]
 	do
-		awk -v input=$PID '$2==input {print $3;}' $FILE > tmp.txt
-		echo "PID=" > tmp.sh
+		awk -v input=$PID '$2==input {print $3}' $FILE > tmp.txt
+		echo -n PID=" > tmp.sh
 		cat tmp.txt >> tmp.sh
 		source tmp.sh
 		echo $PID
