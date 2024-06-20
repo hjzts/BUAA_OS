8c4e83288046e5a0aa882afe38c97a1f259ac0a6
胡健 <22373058@buaa.edu.cn>
2024-06-20 17:51:10 +0800

    delete extra output to test the condition running tests

--- user/false.c
+++ user/false.c
@@ -1,6 +1,6 @@
 #include <lib.h>
 int main(int argc, char **argv) {
     debugk_user("[x]-->PROGRAM user/false.c main() is used");
-    debugf("false!!!\n");
+    // debugf("false!!!\n");
     return 1;
 }
